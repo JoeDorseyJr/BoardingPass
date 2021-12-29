9dc48c6b8e4a6d2ca37cb583acd0029ddd3b7cb0
Joe Dorsey <joedorseyjr@gmail.com>
2021-12-28 20:47:13 -0500

    Created Constructor, Setters and Getters

--- src/Boarding_Pass.java
+++ src/Boarding_Pass.java
@@ -1,3 +1,77 @@
+import java.util.Calendar;
+
 public class Boarding_Pass {
-    //TODO output a file with the following • Boarding Pass Number, Date, Origin, Destination, Estimated time of arrival (ETA), Departure Time
+    private String boardingPassNumber;
+    private Calendar date;
+    private Calendar departureTime;
+    private String origin;
+    private String destination;
+    private Calendar eta;
+
+    Boarding_Pass(){}
+
+    Boarding_Pass(String boardingPassNumber, Calendar date, Calendar departureTime, String origin ,String destination){
+        this.boardingPassNumber = boardingPassNumber;
+        this.date = date;
+        this.departureTime = departureTime;
+        this.origin = origin;
+        this.destination = destination;
+    }
+
+    //Setters
+    public void setBoardingPassNumber(String boardingPassNumber){
+        this.boardingPassNumber = boardingPassNumber;
+    }
+
+    public void setDate(Calendar date){
+        this.date = date;
+    }
+
+    public void setDepartureTime(Calendar departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public void setEta(Calendar eta) {
+        this.eta = eta;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    //Getters
+
+    public Calendar getDate() {
+        return this.date;
+    }
+
+    public Calendar getDepartureTime() {
+        return this.departureTime;
+    }
+
+    public Calendar getEta() {
+        return this.eta;
+    }
+
+    public String getBoardingPassNumber() {
+        return this.boardingPassNumber;
+    }
+
+    public String getDestination() {
+        return this.destination;
+    }
+
+    public String getOrigin() {
+        return this.origin;
+    }
+
+    //Methods
+    public Calendar calcEta(String origin, String destination, Calendar departureTime){
+        //TODO create a list of origins and destinations to have set distances between airports.
+        return null;
+    }
 }

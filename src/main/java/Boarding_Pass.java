ccd4cc7475aff41dee5885103e768b566072c5f6
Joe Dorsey <joedorseyjr@gmail.com>
2022-01-02 14:20:45 -0500

    Updated Boarding_Pass Class.

--- src/main/java/Boarding_Pass.java
+++ src/main/java/Boarding_Pass.java
@@ -1,21 +1,25 @@
+import org.json.simple.JSONObject;
+
 import java.util.Calendar;
 
 public class Boarding_Pass {
     private String boardingPassNumber;
     private Calendar date;
     private Calendar departureTime;
-    private String origin;
-    private String destination;
+    private JSONObject origin;
+    private JSONObject destination;
     private Calendar eta;
+    private double distance;
 
     Boarding_Pass(){}
 
-    Boarding_Pass(String boardingPassNumber, Calendar date, Calendar departureTime, String origin, String destination){
+    Boarding_Pass(String boardingPassNumber, Calendar date, Calendar departureTime, JSONObject origin, JSONObject destination){
         this.boardingPassNumber = boardingPassNumber;
         this.date = date;
         this.departureTime = departureTime;
         this.origin = origin;
         this.destination = destination;
+        calculateDistance();
         calcEta(origin,destination,departureTime);
         storeData();
     }
@@ -33,11 +37,12 @@ public class Boarding_Pass {
 
     public void setDepartureTime(int year, int month, int date, int hourOfDay, int minute) {
         Calendar departureTime = null;
+        assert false;
         departureTime.set(year,month,date,hourOfDay,minute);
         this.departureTime = departureTime;
     }
 
-    public void setDestination(String destination) {
+    public void setDestination(JSONObject destination) {
         this.destination = destination;
     }
 
@@ -45,7 +50,7 @@ public class Boarding_Pass {
         this.eta = eta;
     }
 
-    public void setOrigin(String origin) {
+    public void setOrigin(JSONObject origin) {
         this.origin = origin;
     }
 
@@ -66,13 +71,20 @@ public class Boarding_Pass {
         return this.boardingPassNumber;
     }
 
-    public String getDestination() {
+    public JSONObject getDestination() {
         return this.destination;
     }
 
-    public String getOrigin() {
+    public JSONObject getOrigin() {
         return this.origin;
     }
+
+    /* ASSUMPTIONS: There are few assumptions made to complete the next few calculations.
+   1. Every plane is a Boeing 747 because it's one of the most sold planes in the world.
+   2. Every flight is full so the fuel efficiency will be calculated accordingly. (To determine price possibly)
+   3. A 747 transporting 500 people one-mile uses five gallons of fuel. That means the plane is burning 0.01 gallons per person per mile.
+   4. The plane travels at 550 mph (900 km/h).
+    */
 
     //Methods
     public Calendar calcEta(){
@@ -80,7 +92,7 @@ public class Boarding_Pass {
         return this.getEta();
     }
 
-    public Calendar calcEta(String origin, String destination, Calendar departureTime){
+    public Calendar calcEta(JSONObject origin, JSONObject destination, Calendar departureTime){
         //TODO create a list of origins and destinations to have set distances between airports.
         return this.getEta();
     }
@@ -88,4 +100,27 @@ public class Boarding_Pass {
     public void storeData(){
         //TODO output the data from the instance to a csv file.
     }
+
+    private void calculateDistance(){
+        double lat1 = Double.parseDouble((String) this.origin.get("latitude"));
+        double lat2 = Double.parseDouble((String) this.destination.get("latitude"));
+        double lon1 = Double.parseDouble((String) this.origin.get("longitude"));
+        double lon2 = Double.parseDouble((String) this.destination.get("longitude"));
+        double earthRadius = 3963.0;// Miles
+
+        this.distance = earthRadius * Math.acos( //convert coordinates to distances
+                Math.cos(Math.toRadians(lat1)) *
+                Math.cos(Math.toRadians(lon1)) *
+                Math.cos(Math.toRadians(lat2)) *
+                Math.cos(Math.toRadians(lon2))
+                +
+                Math.cos(Math.toRadians(lat1)) *
+                Math.sin(Math.toRadians(lon1)) *
+                Math.cos(Math.toRadians(lat2)) *
+                Math.sin(Math.toRadians(lon2))
+                +
+                Math.sin(Math.toRadians(lat1)) *
+                Math.sin(Math.toRadians(lat2)) );
+    }
+
 }

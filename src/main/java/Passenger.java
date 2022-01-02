b0986d0c18bc2275db3b6eff5dcdf6e28c41b18b
Joe Dorsey <joedorseyjr@gmail.com>
2022-01-02 13:09:40 -0500

    Cleaned Up passenger class and added the discount method.

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,47 +1,70 @@
 public class Passenger {
-    String name;
-    String email;
-    int phoneNumber;
-    String gender;
-    int age;
+    private String name;
+    private String email;
+    private int phoneNumber;
+    private String gender;
+    private int age;
+    private double discount = 0.0;
 
+//Setters
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getName() {
-        return this.name;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public String getEmail(String email) {
-        return this.email;
-    }
-
     public void setPhoneNumber(int phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+        this.setDiscount(gender);
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+        this.setDiscount(age);
+    }
+
+    public void setDiscount(int age){
+        if (age <= 12){
+            this.discount += .5;
+        } else if (age >= 60) {
+            this.discount += .6;
+        }
+    }
+
+    public void setDiscount(String gender){
+        if (gender.equals("female")){
+            this.discount += .25;
+        }
+    }
+
+//Getters
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail(String email) {
+        return this.email;
     }
 
     public int getPhoneNumber() {
         return this.phoneNumber;
     }
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
     public String getGender() {
         return this.gender;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public int getAge() {
         return this.age;
+    }
+
+    public double getDiscount(){
+        return this.discount;
     }
 }

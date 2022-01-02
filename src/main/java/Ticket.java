456855b153b86d76b2120c7b280390d683ed9534
Joe Dorsey <joedorseyjr@gmail.com>
2022-01-02 12:35:52 -0500

    Main creates new ticket.

--- src/main/java/Ticket.java
+++ src/main/java/Ticket.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 public class Ticket {
     //This class will call the other classes and glue all the other information together.
     //TODO Get the origin and destination
@@ -5,4 +7,8 @@ public class Ticket {
     //TODO Calculate distance and flight times
     //TODO Establish a multiplier for flight price.
     //TODO output a ticket price.
+
+    Ticket() throws IOException {
+        new AirportData("New York");
+    }
 }

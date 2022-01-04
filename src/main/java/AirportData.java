d1b6191d37102fb57c9ed09f1502b658da325c10
Joe Dorsey <joedorseyjr@gmail.com>
2022-01-03 21:25:21 -0500

    Added API statusCode handling.

--- src/main/java/AirportData.java
+++ src/main/java/AirportData.java
@@ -1,23 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
-
+import env.AccessKeys;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.*;
+
+
 public class AirportData implements AccessKeys {
 
     public HashMap<String, JSONObject> airport = new HashMap<>();
     public List<JSONObject> airportResults = new ArrayList<>();
+    public JSONObject response;
+    private Scanner input = new Scanner(System.in);
+    public Boolean status;
 
     public AirportData(String location) throws IOException {
 
@@ -40,6 +41,7 @@ public class AirportData implements AccessKeys {
         while ((line = br.readLine()) != null) {
             sb.append(line);
         }
+
         br.close();
 
         try {
@@ -47,14 +49,7 @@ public class AirportData implements AccessKeys {
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        /*
-         * Write Methods to handle each code from API.
-         * TODO 1. 200 - Success
-         * TODO 2. 204 - No Content
-         * TODO 3. 400 - Bad Request
-         * TODO 4. 401 - Unauthorized
-         * TODO 5. 404 - Not Found...
-         */
+
     }
 
     String cleanseLocation(String location) {
@@ -66,38 +61,38 @@ public class AirportData implements AccessKeys {
 
     void parseJSON(String responseBody) throws ParseException {
         JSONParser parse = new JSONParser();
-        JSONObject response = (JSONObject) parse.parse(responseBody);
-        JSONArray airports = (JSONArray) response.get("airports");
+        response = (JSONObject) parse.parse(responseBody);
+        status = (Boolean) response.get("status");
 
-        airports.stream()
-                .map(Object::toString)
-                .forEach(n -> {
-                    try {
-                        airportResults.add((JSONObject) parse.parse((String) n));
-                    } catch (ParseException e) {
-                        e.printStackTrace();
-                    }
-                });
+        if (status) {
+            JSONArray airports = (JSONArray) response.get("airports");
+            airports.stream()
+                    .map(Object::toString)
+                    .forEach(n -> {
+                        try {
+                            airportResults.add((JSONObject) parse.parse((String) n));
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                    });
 
-        airportResults.forEach(x -> airport.put((String) x.get("iata"), x));
-
+            airportResults.forEach(x -> airport.put((String) x.get("iata"), x));
+        } else {
+            System.err.println(response.get("statusCode")+" - "+response.get("message"));
+        }
     }
 
     public JSONObject choice() {
         String name;
 
         do {
-            System.out.println("\nChoose An Airport (Airport Code): ");
+            System.out.println("\nPlease select an airport (Airport Code): ");
             airport.forEach((k, v) -> System.out.println(k + " - " + v.get("name").toString()));
-            name = userInput();
+            name = input.nextLine().toUpperCase();
         } while (!airport.containsKey(name));
 
         return airport.get(name);
     }
 
-    private String userInput() {
-        Scanner input = new Scanner(System.in);
-        return input.nextLine().toUpperCase();
-    }
 
 }
